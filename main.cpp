30cb7a8ea7587c46ca28f53159b491c0be1bdbb0
FrMnJ <manuelserratossec38@gmail.com>
2025-02-18 22:19:37 -0600

    feat: add option to provide first and last port of scan

--- main.cpp
+++ main.cpp
@@ -19,31 +19,33 @@ void scanPortTCP(const std::string& ip, int port) {
     closesocket(sock);
 }
 
-// Scans the first 1000 ports sequentially
+// Scans the selected ports sequentially
 int main(const int argc, char *argv[]){
     // Check whether we have enough params
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <ip>" << '\n';
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <ip> <primer-puerto> <ultimo-puerto>" << '\n';
         return 1;
     }
     // If so the second parameter is the target IP
-    std::string targetIP = argv[1];
+    std::string target_ip = argv[1];
+    const long first_port = strtol(argv[2], nullptr, 10);
+    const long last_port = strtol(argv[3], nullptr, 10);
     // Start win-sock API
     WSADATA wsa;
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
         std::cerr << "WSAStartup failed!\n";
         return 1;
     }
-    std::cout << "Escaneando " << targetIP << "...\n";
+    std::cout << "[-] Escaneando " << target_ip << " del puerto "<< first_port << " hasta el puerto " << last_port << "...\n";
     // Create the threads that scans the ports sequentially
     std::vector<std::thread> threads;
-    for (int port = 1; port <= 1000; port++) {
-        threads.emplace_back(scanPortTCP, targetIP, port);
+    for (int port = first_port ; port <= last_port; port++) {
+        threads.emplace_back(scanPortTCP, target_ip, port);
     }
 
     for (auto& t : threads) t.join();
 
     WSACleanup();
-    std::cout << "[-] Escaneo a " << targetIP << " completado.\n";
+    std::cout << "[-] Escaneo a " << target_ip << " completado.\n";
     return 0;
 }
